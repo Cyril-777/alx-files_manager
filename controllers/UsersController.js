34f3e0b93b0a15c46f6dc2a104bab0e4c425b420
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-12-03 20:27:40 +0200

    remaining files

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,26 +1,40 @@
-#!/usr/bin/node
-
+/* eslint-disable import/no-named-as-default */
 import sha1 from 'sha1';
-import DBClient from '../utils/db';
+import Queue from 'bull/lib/queue';
+import dbClient from '../utils/db';
 
-class UsersController {
-  static async postNew(request, response) {
-    const { email, password } = request.body;
-    if (!email) return response.status(400).send({ error: 'Missing email' });
-    if (!password) return response.status(400).send({ error: 'Missing password' });
+const userQueue = new Queue('email sending');
 
-    const user = await DBClient.db.collection('users').findOne({ email });
-    if (user) return response.status(400).send({ error: 'Already exist' });
+export default class UsersController {
+  static async postNew(req, res) {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
 
-    const newUser = {
-      email,
-      password: sha1(password),
-    };
-    const result = await DBClient.db.collection('users').insertOne(newUser);
-    newUser.id = result.insertedId;
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+    const user = await (await dbClient.usersCollection()).findOne({ email });
 
-    return response.status(201).send(newUser);
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
+    }
+    const insertionInfo = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
+    const userId = insertionInfo.insertedId.toString();
+
+    userQueue.add({ userId });
+    res.status(201).json({ email, id: userId });
+  }
+
+  static async getMe(req, res) {
+    const { user } = req;
+
+    res.status(200).json({ email: user.email, id: user._id.toString() });
   }
 }
-
-export default UsersController;
